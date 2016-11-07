0b2e5a8be095d23e1d61ce278a988eb025670589
Francesco Muccilli <notoriux@gmail.com>
2016-11-07 01:19:32 +0100

    (forgot this one in the last commit)

--- XargonLibrary3/src/testground/test08/ExampleSource.java
+++ XargonLibrary3/src/testground/test08/ExampleSource.java
@@ -1,26 +1,31 @@
 package testground.test08;
 
+import java.util.concurrent.ExecutorService;
+
 import it.xargon.events.*;
 
 public class ExampleSource extends EventsSourceImpl {
    @FunctionalInterface @Event
-   public interface FirstEvent {public void raise(int arg1, int arg2);}
-   public static Class<FirstEvent> FIRSTEVENT=FirstEvent.class;
+   public interface FirstEvent {public void with(int arg1, int arg2);}
+   public final static Class<FirstEvent> FIRSTEVENT=FirstEvent.class;
    
    @FunctionalInterface @Event
-   public interface SecondEvent {public void raise(String arg1);}
-   public static Class<SecondEvent> SECONDEVENT=SecondEvent.class;
+   public interface SecondEvent {public void with(String arg1);}
+   public final static Class<SecondEvent> SECONDEVENT=SecondEvent.class;
    
    @FunctionalInterface @Event
-   public interface ThirdEvent {public void raise(long arg1);}
-   public static Class<ThirdEvent> THIRDEVENT=ThirdEvent.class;
+   public interface ThirdEvent {public void with(long arg1);}
+   public final static Class<ThirdEvent> THIRDEVENT=ThirdEvent.class;
    
    public ExampleSource() {
       super();
    }
    
+   @Override
+   protected ExecutorService getThreadPool() {return null;}
+   
    public void testForRaisingEvents() {
-      raise(FIRSTEVENT).raise(43,34);
-      raise(SECONDEVENT).raise("test");
+      raise(FIRSTEVENT).with(43,34);
+      raise(SECONDEVENT).with("test");
    }
 }
